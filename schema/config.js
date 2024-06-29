2755c5eecce903ffe62c531bc38d6d4cebcdcdcd
KSTR014 <kalstr014@gmail.com>
2024-06-29 16:12:52 +0700

    Update config.js

--- schema/config.js
+++ schema/config.js
@@ -1,17 +1,17 @@
 const options = {
-  name : 'Rulzz API',
-  developer: "@Irull2nd",
-  port: 3000,
-  webname: 'Rulzz - API Playground',
-  description: 'Simple Base Rest API',
-  favicon: 'https://cdn.itzpire.site/file/1716296635499-d60db2.jpg'
+  name : 'KAL API',
+  developer: "@KxrNot",
+  port: 80,
+  webname: 'Kal - API Playground',
+  description: 'Simple Rest API',
+  favicon: 'https://cdn.discordapp.com/attachments/1204676700697530414/1256537579654877234/undefined.jpg?ex=66812150&is=667fcfd0&hm=b060d696dd0011835ebade42da3d4a2896adf175eeff966891138c4988ec0d1c&'
 }
 
 
 module.exports = {
   options,
   host: {
-    BASE_URL: 'domain.xyz' 
+    BASE_URL: '' 
 /*
 Example: domain.com
 don't use https, just the domain name!
