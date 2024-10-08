e5a48d8efe5417818b22dd17eebcae2e9db31b4f
KSTR014 <kalstr014@gmail.com>
2024-10-09 01:53:53 +0700

    Update config.js

--- schema/config.js
+++ schema/config.js
@@ -1,17 +1,17 @@
 const options = {
-  name : 'KAL API',
+  name : 'kalstr',
   developer: "@KxrNot",
   port: 80,
   webname: 'Kal - API Playground',
   description: 'Simple Rest API',
-  favicon: 'https://cdn.discordapp.com/attachments/1204676700697530414/1256537579654877234/undefined.jpg?ex=66812150&is=667fcfd0&hm=b060d696dd0011835ebade42da3d4a2896adf175eeff966891138c4988ec0d1c&'
+  favicon: 'https://f.uguu.se/BnzmvaDj.jpg'
 }
 
 
 module.exports = {
   options,
   host: {
-    BASE_URL: 'api.kalstrofc.xyz' 
+    BASE_URL: 'api.stclouds.xyz' 
 /*
 Example: domain.com
 don't use https, just the domain name!
